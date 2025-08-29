d52f387730b361e34d5998476ce8f431941aaa76
student-creative <aryasutariya3@gmail.com>
2025-08-29 22:29:05 +0530

    Url Problem

--- routes/cartRoutes.js
+++ routes/cartRoutes.js
@@ -5,6 +5,8 @@ var router = express.Router();
 
 const cart = require('../controller/cartController');
 const mverifyToken = require('../middleware/verifyToken');
+
+
 router.post('/add',mverifyToken,cart.addToCart);
 router.post('/clear',mverifyToken, cart.clearCart);
 router.post('/remove',mverifyToken, cart.removeItem);
